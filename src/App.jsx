c45b1073af4fe6fbcb7271a76302903c280eff9f
aryaman22-hubb <aryamanpanwar187@gmail.com>
2025-03-29 09:37:00 +0530

    Assignment Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
+import { useState } from "react";
 import UpdateItem from "./components/UpdateItem";
 
-// use the following link to get the data
-// `/doors` will give you all the doors, to get a specific door use `/doors/1`.
-const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
-
 function App() {
-  // Get the existing item from the server
-  // const [item, setItem] = useState(null);
-  // pass the item to UpdateItem as a prop
+  const [itemId] = useState(1); // Set the item ID dynamically if needed
 
-  return <UpdateItem />;
+  return <UpdateItem itemId={itemId} />;
 }
 
 export default App;
